5f061f47f4ab377fd33f725e39858367af6e064a
anantjain047 <jainanant0905@gmail.com>
2025-10-07 18:47:53 +0530

    .resetPasswordFunctionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,36 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import { getCurrentUser } from './store/slices/authSlice';
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { getCurrentUser } from "./store/slices/authSlice";
 
 // Import components
-import Navbar from './components/Navbar';
-import ProtectedRoute from './components/ProtectedRoute';
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 // Import pages
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import RoleDashboard from './pages/RoleDashboard';
-import BooksPage from './pages/BooksPage';
-import BookDetailsPage from './pages/BookDetailsPage';
-import ProfilePage from './pages/ProfilePage';
-import AdminDashboard from './pages/AdminDashboard';
-import LibrarianDashboard from './pages/LibrarianDashboard';
-import StudentDashboard from './pages/StudentDashboard';
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import RoleDashboard from "./pages/RoleDashboard";
+import BooksPage from "./pages/BooksPage";
+import BookDetailsPage from "./pages/BookDetailsPage";
+import ProfilePage from "./pages/ProfilePage";
+import AdminDashboard from "./pages/AdminDashboard";
+import LibrarianDashboard from "./pages/LibrarianDashboard";
+import StudentDashboard from "./pages/StudentDashboard";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 
 function App() {
   useEffect(() => {
     // Check if user is already logged in
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       store.dispatch(getCurrentUser());
     }
@@ -86,7 +93,7 @@ function App() {
             <Route
               path="/librarian"
               element={
-                <ProtectedRoute requiredRole={['librarian', 'admin']}>
+                <ProtectedRoute requiredRole={["librarian", "admin"]}>
                   <LibrarianDashboard />
                 </ProtectedRoute>
               }
@@ -99,6 +106,14 @@ function App() {
                 </ProtectedRoute>
               }
             />
+            <Route
+              path="/forgot-password"
+              element={<ForgotPasswordPage />}
+            ></Route>
+            <Route
+              path="/reset-password/:token"
+              element={<ResetPasswordPage />}
+            ></Route>
 
             {/* Catch all route */}
             <Route path="*" element={<Navigate to="/" replace />} />
